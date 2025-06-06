f88376a7e762b92f21f76d53b1b22c34a21eebb7
mazek1 <madskildelund@gmail.com>
2025-06-06 14:36:01 +0200

    Update App.py

--- App.py
+++ App.py
@@ -9,15 +9,15 @@ st.set_page_config(page_title="ZPP Skabelon-Udfylder", layout="centered")
 st.markdown("""
     <style>
         html, body, .main, .block-container {
-            padding-top: 1rem !important;
-            padding-bottom: 0.5rem !important;
+            padding-top: 0.2rem !important;
+            padding-bottom: 0.2rem !important;
         }
         .block-container {
-            padding-top: 2rem !important;
-            padding-bottom: 1rem !important;
+            padding-top: 0.2rem !important;
+            padding-bottom: 0.2rem !important;
         }
         [data-testid="stFileUploader"] {
-            padding: 0.5rem !important;
+            padding: 0.2rem !important;
             margin-bottom: 0.2rem !important;
         }
         
