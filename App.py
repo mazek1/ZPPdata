d293595c5f21405773ff94af9adced6663837e66
mazek1 <madskildelund@gmail.com>
2025-06-06 14:47:56 +0200

    Update App.py

--- App.py
+++ App.py
@@ -9,16 +9,16 @@ st.set_page_config(page_title="ZPP Skabelon-Udfylder", layout="centered")
 st.markdown("""
     <style>
         html, body, .main, .block-container {
-            padding-top: 0rem !important;
-            padding-bottom: 0rem !important;
+            padding-top: 2rem !important;
+            padding-bottom: 0.2rem !important;
         }
         .block-container {
-            padding-top: 2rem !important;
-            padding-bottom: 0rem !important;
+            padding-top: 0.2rem !important;
+            padding-bottom: 0.2rem !important;
         }
         [data-testid="stFileUploader"] {
-            padding: 0rem !important;
-            margin-bottom: 0rem !important;
+            padding: 0.2rem !important;
+            margin-bottom: 0.2rem !important;
         }
         
         
@@ -27,24 +27,27 @@ st.markdown("""
             text-align: center;
             font-size: 1.5em;
             font-weight: 700;
-            margin: 0em 0 0em 0;
+            margin: 0.2em 0 0.3em 0;
         }
         .subtitle {
             text-align: center;
             font-size: 0.9em;
             color: #444;
-            margin-bottom: 0em;
+            margin-bottom: 0.6em;
         }
         h5 {
-            margin: 0rem 0 0rem 0 !important;
+            margin: 0.2rem 0 0.2rem 0 !important;
         }
         hr {
-            margin: 0rem 0 !important;
+            margin: 0.6rem 0 !important;
         }
         /* Lavere højde kun for de nederste uploadfelter */
         div[data-testid="stFileUploader"] label {
-            min-height: 20px !important;
-            padding: 0rem 0rem !important;
+            min-height: 80px !important;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 0.2rem 0.5rem !important;
             margin: 0 !important;
         }
 </style>
